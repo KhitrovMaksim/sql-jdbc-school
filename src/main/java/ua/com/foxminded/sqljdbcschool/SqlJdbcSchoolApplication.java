4ba6d88677f4942e9d35cb7412ba90e8779e09ac
Maksim Khitrov <khitrov.maks@gmail.com>
2020-05-04 18:02:02 +0300

    initial commit

---
+++ src/main/java/ua/com/foxminded/sqljdbcschool/SqlJdbcSchoolApplication.java
@@ -0,0 +1,13 @@
+package ua.com.foxminded.sqljdbcschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlJdbcSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlJdbcSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/foxminded/sqljdbcschool/SqlJdbcSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.foxminded.sqljdbcschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlJdbcSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
