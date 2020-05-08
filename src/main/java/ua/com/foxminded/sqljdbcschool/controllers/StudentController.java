6c6b731b15666e60cf38113a163892284064ae44
Maksim Khitrov <khitrov.maks@gmail.com>
2020-05-08 13:02:19 +0300

    added student group editing

--- src/main/java/ua/com/foxminded/sqljdbcschool/controllers/StudentController.java
+++ src/main/java/ua/com/foxminded/sqljdbcschool/controllers/StudentController.java
@@ -1,9 +1,14 @@
 package ua.com.foxminded.sqljdbcschool.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,5 +40,18 @@ public class StudentController {
         Students studens = new Students(first_name, last_name, group);
         studentsRepository.save(studens);
         return "redirect:/students";
+    }
+    
+    @GetMapping("/students/{id}/edit")
+    public String studentEdit(@PathVariable(value = "id") int id, Model model) {
+        if(!studentsRepository.existsById(id)) {
+            return "redirect:/students";
+        }
+        model.addAttribute("title", "Student profile editing");
+        Optional<Students> studens = studentsRepository.findById(id);
+        List<Students> result = new ArrayList<>();
+        studens.ifPresent(result::add);
+        model.addAttribute("student", result);
+        return "student-edit";
     }
 }
